b7c4578aee0d009fc0647f5441533a6f03b2698c
aj77hub <133131092+aj77hub@users.noreply.github.com>
2025-09-01 15:52:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 //if you need a solid color on the background use below line and disable the Texture loader 
 renderer.setClearColor(  0x404040 ) ;
 document.body.appendChild( renderer.domElement );
+const scene = new THREE.Scene();
 
-  const loader = new GLTFLoader();
 loader.load( './MushroomHouse.glb', function ( gltf ) {
   scene.add( gltf.scene );
 }, undefined, function ( error ) {
@@ -19,7 +19,7 @@ loader.load( './MushroomHouse.glb', function ( gltf ) {
 } );
 
 
-const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 0, 2);
 camera.lookAt(0, 0, 0);// Makes the camera look at the origin.
