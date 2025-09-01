1dc26dc597530dac22b8a796398006f4a1b8fb21
aj77hub <133131092+aj77hub@users.noreply.github.com>
2025-09-01 16:00:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,25 @@
-import * as THREE from 'three'; 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; 
+import * as THREE from 'three';
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-
-const loader = new GLTFLoader();
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-//if you need a solid color on the background use below line and disable the Texture loader 
-renderer.setClearColor(  0x404040 ) ;
+renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
-const scene = new THREE.Scene();
 
-
-
-
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 2);
-camera.lookAt(0, 0, 0);// Makes the camera look at the origin.
-
-
-const controls = new OrbitControls( camera, renderer.domElement );
-
-
-
-
-
-// Cube Model
-const geometry = new THREE.BoxGeometry( 0,0,0 );
-const material = new THREE.MeshPhongMaterial( { color: 0xFF7F50 } );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+camera.position.z = 5;
 
-//Light is added
-// Ambient Light setup
-const light = new THREE.AmbientLight(0xFFFFFF, 3); // initial brightness set to 3
-scene.add(light);
-
-
-
-
-   
-
-
-//This will Render the scene so you can see the object
 function animate() {
-  
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
   renderer.render( scene, camera );
+
 }
-renderer.setAnimationLoop( animate );
