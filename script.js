dca69cb17753e73def6f9a040c8bcdb312af3350
aj77hub <133131092+aj77hub@users.noreply.github.com>
2025-09-04 07:30:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,19 +15,22 @@ document.body.appendChild( renderer.domElement );
 const scene = new THREE.Scene();
 
 // Create a TextureLoader
-const loaderT = new THREE.TextureLoader();
+
 //Load the image and set it as the scene background !! Make Sure the image is from HTTPS
+
+const loaderT = new THREE.TextureLoader();
+
+
 loaderT.load('https://t4.ftcdn.net/jpg/04/33/16/71/360_F_433167186_bnAhGZ4fANlmExoSXw4EagCsfVbmAPIc.jpg', 
-
-             //imgru file  "https://i.imgur.com/z117eQD.jpeg"
-             
-
+     
     function(texture) {
   
     scene.background = texture;
 }, undefined, function(err) {
     console.error('An error occurred loading the background image:', err);
 });
+
+// use path like this for local files.  "./asset/Navy-BG.jpg"
 loaderT.load('https://t4.ftcdn.net/jpg/04/33/16/71/360_F_433167186_bnAhGZ4fANlmExoSXw4EagCsfVbmAPIc.jpg',
   function (texture) {
     texture.encoding = THREE.sRGBEncoding;
@@ -116,7 +119,7 @@ brightnessSlider.addEventListener('input', (event) => {
 
     loader.load(
         // Path to your .gltf or .glb file
-        'https://cdn.tinyglb.com/models/c059175b3c22405e90d5d5a284fa2de5.glb',      
+        './mushroom_house_1.glb',      
       
         // Called when the resource is loaded
         function (gltf) {
