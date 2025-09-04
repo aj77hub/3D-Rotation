64a4dbeeefe392d6c00bae0b9e6bd008b92a85d2
aj77hub <133131092+aj77hub@users.noreply.github.com>
2025-09-04 07:37:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ brightnessSlider.addEventListener('input', (event) => {
 
     loader.load(
         // Path to your .gltf or .glb file
-        './mushroom_house_1.glb',      
+        'https://cdn.tinyglb.com/models/c059175b3c22405e90d5d5a284fa2de5.glb',      
       
         // Called when the resource is loaded
         function (gltf) {
