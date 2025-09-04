0c1bc2889373b9366d52a626e36b90ab3211c05b
aj77hub <133131092+aj77hub@users.noreply.github.com>
2025-09-04 13:00:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-
 import * as THREE from 'three'; 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; 
-
-
 
 const loader = new GLTFLoader();
 const renderer = new THREE.WebGLRenderer();
